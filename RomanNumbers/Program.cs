f56055049cb58195c667d78abddd84b8405458c5
Simon Bochkarev <kemer0v0@mail.ru>
2022-02-24 22:40:44 +0700

    Добавьте файлы проекта.

---
+++ RomanNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RomanNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                RomanNumber obj1 = new RomanNumber(32);
+                RomanNumber obj2 = new RomanNumber(4);
+                RomanNumber obj3 = new RomanNumber(13);
+                RomanNumber obj4 = new RomanNumber(26);
+                Console.WriteLine($"Сложение: {obj3.ToString()} + {obj4.ToString()} = {RomanNumber.Add(obj3, obj4).ToString()}");
+                Console.WriteLine($"Деление: {obj1.ToString()} / {obj2.ToString()} = {RomanNumber.Div(obj1, obj2).ToString()}");
+                Console.WriteLine($"Вычитание: {obj1.ToString()} - {obj4.ToString()} = {RomanNumber.Sub(obj1, obj4).ToString()}");
+                Console.WriteLine($"Умножение: {obj2.ToString()} * {obj3.ToString()} = {RomanNumber.Mul(obj2, obj3).ToString()}");
+                RomanNumber[] objects = { obj1, obj2, obj3, obj4 };
+                Array.Sort(objects);
+                Console.WriteLine("Результат сортировки");
+                foreach (var value in objects)
+                {
+                    Console.Write(value.ToString() + " ");
+                }
+
+            }
+            catch (RomanNumberException exception)
+            {
+                Console.WriteLine($"Ошибка: {exception.Message}");
+            }
+
+        }
+    }
+}
---
+++ RomanNumbers/RomanNumbers.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumbers
+{
+    internal class RomanNumber : ICloneable, IComparable
+    {
+
+        private ushort number;
+        private static int[] values = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        private static string[] roman = new string[]
+            { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+        public RomanNumber(ushort n)
+        {
+            if (n <= 0) throw new RomanNumberException($"Число {n} меньше либо равно 0");
+            else this.number = n;
+        }
+
+        public static RomanNumber Add(RomanNumber? n1, RomanNumber? n2)
+        {
+            int res = n1.number + n2.number;
+            if (res <= 0) throw new RomanNumberException("Не удалось сложить  числа!");
+            else
+            {
+                RomanNumber result = new((ushort)res);
+                return result;
+            }
+        }
+
+        public static RomanNumber Sub(RomanNumber? n1, RomanNumber? n2)
+        {
+            int res = n1.number - n2.number;
+            if (res <= 0) throw new RomanNumberException("Результат вычитания меньше или равен 0");
+            else
+            {
+                RomanNumber result = new((ushort)res);
+                return result;
+            }
+        }
+   
+        public static RomanNumber Mul(RomanNumber? n1, RomanNumber? n2)
+        {
+            int res = n1.number * n2.number;
+            if (res <= 0) throw new RomanNumberException("Не удалось умножить числа");
+            else
+            {
+                RomanNumber result = new((ushort)res);
+                return result;
+            }
+        }
+
+        public static RomanNumber Div(RomanNumber? n1, RomanNumber? n2)
+        {
+
+            if (n2.number == 0) throw new RomanNumberException("Не удалось разделить числа");
+            else
+            {
+                int res = n1.number / n2.number;
+                if (res == 0) throw new RomanNumberException("Не удалось разделить числа");
+                else
+                {
+                    RomanNumber result = new((ushort)res);
+                    return result;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            int tmp = number;
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < 13; i++)
+            {
+                while (tmp >= values[i])
+                {
+                    tmp -= (ushort)values[i];
+                    result.Append(roman[i]);
+                }
+            }
+            if (result.ToString() == "")
+                throw new RomanNumberException("Перевод числа в Римские цифры невозможен");
+            else
+                return result.ToString();
+
+        }
+
+        public object Clone()
+        {
+            return new RomanNumber(number);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is RomanNumber roman)
+                return number.CompareTo(roman.number);
+            else
+                throw new RomanNumberException("object is not a RomanNumber");
+        }
+
+    }
+
+}
